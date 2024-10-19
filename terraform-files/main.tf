5be070d230a3f572b0f08ad7ef54fda47346d7b8
priyacharles1717 <epcipa1793@gmail.com>
2024-10-19 23:27:33 +0530

    Updated main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "test-server" {
   ami = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
   key_name = "Project"
-  vpc_security_group_ids = ["sg-04cf4e3801bd09203"]
+  vpc_security_group_ids = ["sg-098956748897827fa"]
   connection {
      type = "ssh"
      user = "ec2-user"
